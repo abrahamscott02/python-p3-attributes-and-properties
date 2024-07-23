a71c3eb6217f9db6e5f5f7e1872d4b363af09fab
Abraham Scott <abrahamscott02@gmail.com>
2024-07-23 12:21:22 -0600

    Done

--- lib/dog.py
+++ lib/dog.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 APPROVED_BREEDS = [
     "Mastiff",
     "Chihuahua",
@@ -12,4 +10,30 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name="Dog", breed="Pointer"):
+        self._name = None  # Initialize _name to None
+        self._breed = None  # Initialize _breed to None
+        self.name = name  # Calls the name setter
+        self.breed = breed  # Calls the breed setter
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if type(value) == str and 1 <= len(value) <= 25:
+            self._name = value
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    @property
+    def breed(self):
+        return self._breed
+
+    @breed.setter
+    def breed(self, value):
+        if value in APPROVED_BREEDS:
+            self._breed = value
+        else:
+            print("Breed must be in list of approved breeds.")
